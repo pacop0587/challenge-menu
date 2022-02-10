650ca9372e69a66c2b7f2a827746c5f7f3ff8c23
Paco <pacop0587@gmail.com>
2022-02-09 22:04:32 -0600

    Se agrego axios para obtener token de sesion

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import validator from "validator";
+import axios from "axios";
 import Error from "./components/Error";
+
 
 function App() {
 	//Hooks 
@@ -23,54 +25,63 @@ function App() {
 			return
 		}
 
-		console.log('Campos completados!!!!');
+		axios.post('http://challenge-react.alkemy.org/', {
+			email: email,
+			password: password
+			})
+			.then((response) => {
+			console.log(response.data.token)
+			})
+			.catch((error) => {
+			console.log(error)
+			})
+
 		setError(false);
 		setErrorEmail(false);
+
+
 	}
+
 
 
   return (
     <div className="vh-100 gradient-custom">
 		<div className="container py-5 h-100">
-			<div className="row d-flex justify-content-center align-items-center h-100">
-				<div className="col-12 col-md-8 col-lg-6 col-xl-5">
-					<div className="card bg-dark text-white">
-						<div className="card-body p-5 text-center">
-							<div className="mb-md-4 mt-md-4 pb-2">
-								{
-									error && <Error mensaje='Debes completar los campos' />  
-								}	  
-								{
-									errorEmail && <Error mensaje='El email no tiene el formato correcto' />
-								}
-								<h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-								<p className="text-white-50 mb-5">Por favor, ingresa tu correo y contraseña</p>
-								<div className="form-outline form-white mb-4">
-									<input
-										type="email"
-										className="form-control form-control-lg"
-										onChange={(e) => setEmail(e.target.value)}
-									  />
-									<label className="form-label mt-2">Correo</label>
+			  <div className="row d-flex justify-content-center align-items-center h-100">
+			  	<div className="col-12 col-md-8 col-lg-6 col-xl-5">
+					<form onSubmit={validarLogin}>
+						<div className="card bg-dark text-white">
+							<div className="card-body p-5 text-center">
+								<div className="mb-md-4 mt-md-4 pb-2">
+									{
+										error && <Error mensaje='Debes completar los campos' />  
+									}	  
+									{
+										errorEmail && <Error mensaje='El email no tiene el formato correcto' />
+									}
+									<h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+									<p className="text-white-50 mb-5">Por favor, ingresa tu correo y contraseña</p>
+									<div className="form-outline form-white mb-4">
+										<input
+											type="email"
+											className="form-control form-control-lg"
+											onChange={(e) => setEmail(e.target.value)}
+										/>
+										<label className="form-label mt-2">Correo</label>
+									</div>
+									<div className="form-outline form-white mb-4">
+										<input
+											type="password"
+											className="form-control form-control-lg"
+											onChange={(e) => setPassword(e.target.value)}  
+										/>
+										<label className="form-label mt-2" >Contraseña</label>
+									</div>
+									<button className="btn btn-outline-light btn-lg px-5 mt-3" type="submit">Enviar</button>
 								</div>
-								<div className="form-outline form-white mb-4">
-									<input
-										type="password"
-										className="form-control form-control-lg"
-										onChange={(e) => setPassword(e.target.value)}  
-									  />
-									<label className="form-label mt-2" >Contraseña</label>
-								</div>
-								<button
-									className="btn btn-outline-light btn-lg px-5 mt-3"
-									type="submit"
-									onClick={validarLogin}
-								>
-									Enviar
-								</button>
 							</div>
 						</div>
-					</div>
+					</form>
 				</div>
 			</div>
 		</div>
